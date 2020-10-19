110d80c1fe444de9a84c22332411cf8ff7026d82
rulofburger <rulofburger@gmail.com>
2020-10-19 20:02:56 +0200

    Upload examples to be used in lecture

--- shrinkage examples.R
+++ shrinkage examples.R
@@ -16,7 +16,8 @@ df_NIDSsmall <- read_dta("NIDSsmall.dta") %>%
   mutate_at(vars(7:15), as_factor) %>% 
   mutate_at(vars(16:ncol(.)), as.double) %>% 
   mutate(
-    log_wage = log(wages)
+    log_wage = log(wages),
+    log_earnings = log(earnings)
   ) %>% 
   select(-c('wages', 'earnings'))
 
@@ -27,13 +28,15 @@ df_regr <- df_NIDSsmall %>%
   filter(tenure > 0) %>% 
   filter(hours > 0) %>% 
   select(-c('status', 'occupation', 'industry', 'dc', 'female', 'marital', 'race')) %>% 
-  select(-c('pid', 'tenure', 'educ2', 'age2')) %>% 
+  select(-c('pid', 'tenure', 'educ2', 'age2', 'log_wage')) %>% 
   mutate_at(vars(1:ncol(.)), as.double) %>% 
+  mutate(union = if_else(union == 6, 0, 1)) %>% 
   filter(complete.cases(.))
 
-lm_fit <- lm(data = df_regr, formula = log_wage ~ .)
+lm_fit <- lm(data = df_regr, formula = log_earnings ~ .)
+summary(lm_fit)
 
-ridge_model <- glmnet::glmnet(y = df_regr$log_wage, x = df_regr %>% select(-log_wage) %>% as.matrix, alpha = 0, lamda = 1)
+ridge_model <- glmnet::glmnet(y = df_regr$log_earnings, x = df_regr %>% select(-log_earnings) %>% as.matrix, alpha = 0, lamda = 1)
 
 tmp <- as.data.frame(as.matrix(coef(ridge_model)))
 tmp$coef <- row.names(tmp)
@@ -50,7 +53,7 @@ ggplot(tmp[tmp$coef != "(Intercept)", ], aes(lambda, value, color = coef, linety
   theme(legend.key.width = unit(3, "lines"))
 
 
-lasso_model <- glmnet::glmnet(y = df_regr$log_wage, x = df_regr %>% select(-log_wage) %>% as.matrix, alpha = 1, lamda = 0.1)
+lasso_model <- glmnet::glmnet(y = df_regr$log_earnings, x = df_regr %>% select(-log_earnings) %>% as.matrix, alpha = 1, lamda = 0.1)
 
 tmp <- as.data.frame(as.matrix(coef(lasso_model)))
 tmp$coef <- row.names(tmp)
@@ -67,13 +70,17 @@ ggplot(tmp[tmp$coef != "(Intercept)", ], aes(lambda, value, color = coef, linety
   theme(legend.key.width = unit(3, "lines"))
 
 model_rpart_simple <- rpart::rpart(
-  data = df_regr %>% select(-hours),
-  formula = log_wage ~ ., 
+  data = df_regr,
+  formula = log_earnings ~ ., 
   method = "anova",
-  control = rpart::rpart.control(cp = 0.01)
+  control = rpart::rpart.control(cp = 0.015)
 )
 
 model_rpart_simple
 fancyRpartPlot(model_rpart_simple, main = "Regression Tree for NIDS Income")
 
+
+model_knn <- knnreg(y = df_regr$log_earnings, x = df_regr %>% select(-log_earnings) %>% as.matrix, k = 5)
+
+plot(df_regr$log_earnings, predict(model_knn, df_regr  %>% select(-log_earnings) %>% as.matrix))
 
